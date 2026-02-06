04081c451aa671c8875ff6d758d36b84bc507703
abhijal07 <abhijalchalliyil07@gmail.com>
2026-02-06 23:42:27 +0530

    program to check if array is sorted

---
+++ src/CheckArraySorted.java
@@ -0,0 +1,22 @@
+// Checks whether an array is sorted in ascending order.
+
+public class CheckArraySorted {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 5, 4};
+
+        boolean isSorted = true;
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                isSorted = false;
+                break;
+            }
+        }
+
+        if (isSorted) {
+            System.out.println("Array is sorted");
+        } else {
+            System.out.println("Array is not sorted");
+        }
+    }
+}
